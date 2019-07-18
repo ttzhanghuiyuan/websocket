bb8989ae56818458d0e0da8cb1991523221635dd
abner <abner@qq.com>
2019-07-18 12:16:52 +0800

    websocket

---
+++ send_websocket.php
@@ -0,0 +1,3 @@
+<?php
+file_get_contents('http://127.0.0.1:9501/admin_notify/send_message?message=10000');
+echo "over! \n";
---
+++ websocket_test.php
@@ -0,0 +1,33 @@
+<?php
+
+$server = new Swoole\WebSocket\Server("0.0.0.0", 9501);
+$server->on('open', function (Swoole\WebSocket\Server $server, $request) {
+    echo "server: handshake success with fd{$request->fd}\n";
+});
+$server->on('message', function (Swoole\WebSocket\Server $server, $frame) {
+    echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
+    $server->push($frame->fd, "this is server");
+});
+$server->on('close', function ($ser, $fd) {
+    echo "client {$fd} closed\n";
+});
+$server->on('request', function (Swoole\Http\Request $request, Swoole\Http\Response $response) {
+    global $server;//调用外部的server
+    // $server->connections 遍历所有websocket连接用户的fd，给所有用户推送
+
+    $path_info=$request->server["path_info"];
+    echo $path_info."\n";
+
+    echo $request->get['message']."\n";
+
+    foreach ($server->connections as $fd) {
+        // 需要先判断是否是正确的websocket连接，否则有可能会push失败
+        if ($server->isEstablished($fd)) {
+            $server->push($fd, $request->get['message']);
+        }
+
+    }
+
+    $response->end("SUCC");
+});
+$server->start();
